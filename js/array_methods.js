eafda886484b90c51e583aea84bd46e44f0d88d6
stholmes <stephenshigemi@gmail.com>
2020-02-07 17:44:19 -0800

    Change to arrow functions

--- js/array_methods.js
+++ js/array_methods.js
@@ -1,3 +1,6 @@
+
+let testArr = [0,1,2,3,4,5]
+
 //Push Front
 
 pushFront =(arr, val) =>{
@@ -9,6 +12,37 @@ pushFront =(arr, val) =>{
     return arr;
 }
 
+popFront = (arr) =>{
+    let returnVal = arr[0];
+    for(let i = 0; i < arr.length-1; i++){
+        arr[i]= arr[i+1];
+    }
+    arr.pop();
+    return returnVal;
+}
 
-let test = [1,2,3,4,5]
-console.log(pushFront(test,0))
+// let x= popFront(testArr);
+// console.log(x)
+// console.log(testArr)
+
+
+insertAt = (arr, index, val) =>{
+    arr.push(val);
+    for(let i = arr.length-1;i>index;i--){
+        let temp = arr[i];
+        arr[i] = arr[i-1];
+        arr[i-1] = temp;
+    }
+    return arr;
+}
+// let x = insertAt(testArr,1,8);
+// console.log(x)
+
+removeAt = (arr,index)=>{
+    let returnVal = arr[index];
+    for(let i = index; i<arr.length-1; i++){
+        arr[i] = arr[i+1];
+    }
+    arr.pop();
+    return returnVal;
+}
