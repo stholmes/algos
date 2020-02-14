bf93a589cfe9f9a9c1cef4eb9dd465b8a1586719
stholmes <stephenshigemi@gmail.com>
2020-02-13 16:08:27 -0800

    Add factorial recursive

--- python/recursive.py
+++ python/recursive.py
@@ -5,4 +5,11 @@ def fibanocci(num):
         return 1
     return fibanocci(num -1 )+ fibanocci(num-2)
 
-print(fibanocci(7))
+def factorial(num):
+    if(num == 0):
+        return 0
+    if(num == 1):
+        return 1
+    return num * factorial(num-1)
+
+print(factorial(10))
