f1d9115892f726097f0644cde73736728941badb
stholmes <stephenshigemi@gmail.com>
2020-02-18 20:57:22 -0800

    Add to front function

--- python/list_methods.py
+++ python/list_methods.py
@@ -1,9 +1,20 @@
 def reverseList(list):
-    for i in range(int(len(list)/2)) :
+    midPoint = int(len(list)/2)
+    for i in range(midPoint) :
         temp = list[i];
         list[i] = list[len(list)-1-i];
         list[len(list)-1-i] = temp
     return list
 test = [0,1,2,3,4,5]
-x =  reverseList(test)
-print(x)
+# x =  reverseList(test)
+# print(x)
+
+
+def pushFront(list, val):
+    list.append(None)
+    for i in range(len(list)-1,0,-1):
+        list[i] = list[i-1]
+    list[0] = val
+    return list
+
+print(pushFront(test,22))
